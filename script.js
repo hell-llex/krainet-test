ce623a32210a15747767644135bec5b2c8c5aefa
hell-llex <demeshenko333@gmail.com>
2024-03-10 13:57:31 +0300

    feat: add validation form

--- script.js
+++ script.js
@@ -12,3 +12,61 @@ const menuItem = document.querySelectorAll('.menu__item');
     body.style.overflow = body.style.overflow ? '' : 'hidden';
   });
 })
+
+const form = document.querySelector('.contacts__form');
+const inputName = document.querySelector('input[name="name"]');
+const inputEmail = document.querySelector('input[name="email"]');
+const inputMessage = document.querySelector('textarea[name="message"]');
+const inputPolicy = document.querySelector('input[name="policy"]');
+const contactsBtn = document.querySelector('.contacts__button');
+
+function disableBtnForm() {
+  if (document.documentElement.clientWidth > 425) {
+    if ((!form.elements[0].classList.contains('invalid') && !!form.elements[0].value)
+      && (!form.elements[1].classList.contains('invalid') && !!form.elements[1].value)
+      && (!form.elements[2].classList.contains('invalid') && !!form.elements[2].value)
+    ) {
+      contactsBtn.classList.remove('button_disable');
+    } else {
+      contactsBtn.classList.add('button_disable');
+    }
+  } else {
+    if ((!form.elements[0].classList.contains('invalid') && !!form.elements[0].value)
+      && (!form.elements[1].classList.contains('invalid') && !!form.elements[1].value)
+      && (!form.elements[2].classList.contains('invalid') && !!form.elements[2].value)
+      && inputPolicy.checked
+    ) {
+      contactsBtn.classList.remove('button_disable');
+    } else {
+      contactsBtn.classList.add('button_disable');
+    }
+  }
+}
+
+[inputName, inputEmail, inputMessage].forEach(elem => {
+  elem.addEventListener('input', () => {
+    if (elem.classList.contains('contacts__input-name')) {
+      if (!/^[a-zA-Zа-яА-Я-]{2,}$/.test(inputName.value) && inputName.value) {
+        elem.classList.add('invalid');
+      } else {
+        elem.classList.remove('invalid');
+      }
+    } else if (elem.classList.contains('contacts__input-email')) {
+      if (!/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(inputEmail.value) && inputEmail.value) {
+        elem.classList.add('invalid');
+      } else {
+        elem.classList.remove('invalid');
+      }
+    } else if (elem.classList.contains('contacts__input-message')) {
+      const words = elem.value.split(/\s+/);
+      if (words.length < 3 && elem.value) {
+        elem.classList.add('invalid');
+      } else {
+        elem.classList.remove('invalid');
+      }
+    }
+    disableBtnForm();
+  });
+});
+
+inputPolicy.addEventListener('change', disableBtnForm);
